8a833cd150c6474dbf6786d6c62a111e16b9fc1c
вишня <pnxx.py@gmail.com>
2026-02-15 21:20:12 +0300

    Add files via upload

---
+++ buildozer.spec
@@ -0,0 +1,123 @@
+[app]
+
+# (str) Title of your application
+title = Map2motion
+
+# (str) Package name
+package.name = map2motion
+
+# (str) Package domain (needed for android/ios packaging)
+package.domain = test
+
+# (str) Source code where the main.py live
+source.dir = .
+
+# (list) Source files to include (let empty to include all the files)
+source.include_exts = py,png,jpg,kv,atlas,ttf
+
+# (list) List of inclusions using pattern matching
+#source.include_patterns = assets/*,images/*.png
+source.include_patterns = Assets/**
+
+# (list) Source files to exclude (let empty to not exclude anything)
+#source.exclude_exts = spec
+
+# (list) List of directory to exclude (let empty to not exclude anything)
+#source.exclude_dirs = tests, bin, venv
+
+# (list) List of exclusions using pattern matching
+# Do not prefix with './'
+#source.exclude_patterns = license,images/*/*.jpg
+
+# (str) Application versioning (method 1)
+version = 0.1
+
+# (str) Application versioning (method 2)
+# version.regex = __version__ = ['"](.*)['"]
+# version.filename = %(source.dir)s/main.py
+
+# (list) Application requirements
+# comma separated e.g. requirements = sqlite3,kivy
+requirements = python3==3.10.11,kivy==2.2.1,kivymd==1.1.1,plyer==2.1.0,android
+
+# (str) Custom source folders for requirements
+# Sets custom source for any requirements with recipes
+# requirements.source.kivy = ../../kivy
+
+# (str) Presplash of the application
+#presplash.filename = %(source.dir)s/data/presplash.png
+
+# (str) Icon of the application
+#icon.filename = %(source.dir)s/data/icon.png
+
+# (list) Supported orientations
+# Valid options are: landscape, portrait, portrait-reverse or landscape-reverse
+orientation = portrait
+
+# (list) List of service to declare
+#services = NAME:ENTRYPOINT_TO_PY,NAME2:ENTRYPOINT2_TO_PY
+
+#
+# OSX Specific
+#
+
+#
+# author = © Copyright Info
+
+android.allow_backup = True
+
+# (str) XML file for custom backup rules (see official auto backup documentation)
+# android.backup_rules =
+
+# (str) If you need to insert variables into your AndroidManifest.xml file,
+# you can do so with the manifestPlaceholders property.
+# This property takes a map of key-value pairs. (via a string)
+# Usage example : android.manifest_placeholders = [myCustomUrl:\"org.kivy.customurl\"]
+# android.manifest_placeholders = [:]
+
+# (bool) Skip byte compile for .py files
+# android.no-byte-compile-python = False
+
+# (str) The format used to package the app for release mode (aab or apk or aar).
+# android.release_artifact = aab
+
+# (str) The format used to package the app for debug mode (apk or aar).
+# android.debug_artifact = apk
+
+#
+# Python for android (p4a) specific
+#
+
+# (str) python-for-android URL to use for checkout
+p4a.url = https://github.com/kivy/python-for-android.git
+
+# (str) python-for-android fork to use in case if p4a.url is not specified, defaults to upstream (kivy)
+#p4a.fork = kivy
+
+# (str) python-for-android branch to use, defaults to master
+4a.branch = develop
+
+# (str) python-for-android specific commit to use, defaults to HEAD, must be within p4a.branch
+#p4a.commit = HEAD
+
+# (str) python-for-android git clone directory (if empty, it will be automatically cloned from github)
+#p4a.source_dir =
+
+# (str) The directory in which python-for-android should look for your own build recipes (if any)
+#p4a.local_recipes =
+
+# (str) Filename to the hook for p4a
+#p4a.hook =
+
+# (str) Bootstrap to use for android builds
+# p4a.bootstrap = sdl2
+
+# (int) port number to specify an explicit --port= p4a argument (eg for bootstrap flask)
+#p4a.port =
+
+# Control passing the --use-setup-py vs --ignore-setup-py to p4a
+# "in the future" --use-setup-py is going to be the default behaviour in p4a, right now it is not
+# Setting this to false will pass --ignore-setup-py, true will pass --use-setup-py
+# NOTE: this is general setuptools integration, having pyproject.toml is enough, no need to generate
+# setup.py if you're using Poetry, but you need to add "toml" to source.include_exts.
+#p4a.setup_py = false
---
+++ main.py
@@ -0,0 +1,159 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, SlideTransition
+from kivy.properties import StringProperty, NumericProperty
+from kivy.logger import Logger
+from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.label import MDLabel
+from kivy.factory import Factory
+from screens.start_screen import StartScreen
+import os, time
+
+from screens.camera_screen import CameraScreen
+from screens.line_select_screen import LineSelectScreen
+from screens.profile_screen import ProfileScreen
+from screens.result_screen import ResultScreen
+
+
+class MainApp(MDApp):
+    title_text = StringProperty("Камера")
+    captured_image_path = StringProperty("")
+    min_robot_speed = NumericProperty(0)
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Gray"
+
+        Builder.load_file("app.kv")
+
+        self.sm = ScreenManager(transition=SlideTransition(direction="left"))
+        self.sm.add_widget(StartScreen(name="start"))
+        self.sm.add_widget(CameraScreen(name="camera"))
+        self.sm.add_widget(LineSelectScreen(name="line"))
+        self.sm.add_widget(ProfileScreen(name="profile"))
+        self.sm.add_widget(ResultScreen(name="result"))
+        self.sm.current = "start"
+
+        return self.sm
+    
+
+    def go_next(self, screen_name):
+        self.sm.transition.direction = "left"
+        self.sm.current = screen_name
+        self.update_title(screen_name)
+        
+    def go_back(self, screen_name):
+        self.sm.transition.direction = "right"
+        self.sm.current = screen_name
+        self.update_title(screen_name)
+
+    def update_title(self, screen_name):
+        titles = {
+            "start": "Старт",
+            "camera": "Камера",
+            "line": "Выбор линии",
+            "profile": "Профиль",
+            "result": "Результат"}
+        self.title_text = titles.get(screen_name, "")
+
+    def show_snackbar(self, text):
+        snackbar = Factory.StyledSnackbar()
+        snackbar.ids.label.text = text
+        snackbar.open()
+
+    def get_platform(self):
+        try:
+            from kivy.utils import platform
+            return platform
+        except:
+            return 'unknown'
+
+    def open_camera(self):
+        platform = self.get_platform()
+
+        if platform == "android":
+            self.open_camera_android()
+        elif platform == "ios":
+            self.open_camera_ios()
+        else:
+            self.open_camera_desktop()
+
+    def open_camera_android(self):
+        try:
+            from jnius import autoclass
+
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            Intent = autoclass('android.content.Intent')
+            MediaStore = autoclass('android.provider.MediaStore')
+
+            activity = PythonActivity.mActivity
+            intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            activity.startActivity(intent)
+
+            self.show_snackbar("Сделай фото, затем выбери его из галереи")
+
+        except Exception as e:
+            self.show_snackbar(f"Ошибка запуска камеры: {e}")
+
+    def open_camera_ios(self):
+        try:
+            from plyer import camera
+
+            filename = os.path.join(
+                os.path.expanduser('~'),
+                'Documents',
+                f"photo_{int(time.time())}.jpg"
+            )
+
+            camera.take_picture(
+                filename=filename,
+                on_complete=self.img_ready
+            )
+
+        except Exception as e:
+            Logger.error(f"Camera: Ошибка iOS камеры: {e}")
+            self.show_snackbar(f"Ошибка камеры: {e}")
+
+
+    def open_camera_desktop(self):
+        self.show_snackbar("На ПК камера недоступна. Используй галерею.")
+
+
+    def img_ready(self, filename):
+        if not filename or not os.path.exists(filename):
+            self.show_snackbar("Файл не найден")
+            return
+
+        self.captured_image_path = filename
+
+        try:
+            line_screen = self.sm.get_screen("line")
+            if hasattr(line_screen, "set_image"):
+                line_screen.set_image(filename)
+
+        except Exception as e:
+            self.show_snackbar(f"Ошибка обработки: {e}")
+            return
+        
+        self.go_next("line")
+
+
+    def pick_from_gallery(self):
+        try:
+            from plyer import filechooser
+
+            filechooser.open_file(
+                on_selection=self._on_gallery_select,
+                filters=[("Images", "*.png;*.jpg;*.jpeg")]
+            )
+        except Exception as e:
+            self.show_snackbar(f"Не удалось открыть галерею: {e}")
+
+    def _on_gallery_select(self, selection):
+        if selection:
+            self.img_ready(selection[0])
+
+
+if __name__ == "__main__":
+    MainApp().run()
