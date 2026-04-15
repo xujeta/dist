4ed8f7d127e5764be4f6162d40f86de18cdee014
вишня <pnxx.py@gmail.com>
2026-04-15 10:51:51 +0300

    Delete first_app/screens/screens directory

--- first_app/screens/screens/camera_screen.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class CameraScreen(Screen):
-    def open_gallery(self):
-        print("Галерея пока заглушка")
-
-    def go_next(self):
-        self.manager.parent.go_next("line")
