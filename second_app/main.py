68dcb72d567fd39b6e69f68654eed6b4dc4955bc
вишня <pnxx.py@gmail.com>
2026-04-16 09:45:27 +0300

    Update main.py

--- second_app/main.py
+++ second_app/main.py
@@ -4,6 +4,7 @@ from kivy.clock import Clock, mainthread
 from kivy.utils import platform
 import threading
 import requests
+from android.permissions import request_permissions, Permission
 
 ESP_IP = "192.168.4.1"
 ESP_PORT = 80
@@ -16,20 +17,18 @@ class WiFiRelayApp(MDApp):
         super().__init__(**kwargs)
         self.is_connected = False
         self.level = 0
+        self.command_in_progress = False
 
     def build(self):
-
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Gray"
 
-        if platform == "android":
-            from android.permissions import request_permissions, Permission
-            request_permissions([
-                Permission.INTERNET,
-                Permission.ACCESS_NETWORK_STATE,
-                Permission.ACCESS_WIFI_STATE,
-                Permission.CHANGE_WIFI_STATE
-            ])
+        request_permissions([
+            Permission.INTERNET,
+            Permission.ACCESS_NETWORK_STATE,
+            Permission.ACCESS_WIFI_STATE,
+            Permission.CHANGE_WIFI_STATE
+        ])
 
         return Builder.load_file("app.kv")
 
@@ -37,7 +36,14 @@ class WiFiRelayApp(MDApp):
         Clock.schedule_interval(self.check_status, 2)
 
     def check_status(self, dt):
-        threading.Thread(target=self._fetch_status, daemon=True).start()
+
+        if self.command_in_progress:
+            return
+
+        threading.Thread(
+            target=self._fetch_status,
+            daemon=True
+        ).start()
 
     def _fetch_status(self):
 
@@ -85,7 +91,7 @@ class WiFiRelayApp(MDApp):
         if state:
             btn.md_bg_color = (0,0.8,0,1)
         else:
-            btn.md_bg_color = (0.2,0.2,0.2,1)
+            btn.md_bg_color = (0.33, 0.33, 0.33, 1)  # OFF (#545454)
 
     def update_status(self, text, color):
 
@@ -101,6 +107,7 @@ class WiFiRelayApp(MDApp):
         # мгновенный отклик UI
         old_level = self.level
         self.level = level
+        self.command_in_progress = True
         self.update_ui()
 
         threading.Thread(
@@ -120,6 +127,7 @@ class WiFiRelayApp(MDApp):
             )
 
             if resp.status_code == 200:
+                self.command_in_progress = False
                 self._fetch_status()
 
             else:
@@ -127,6 +135,7 @@ class WiFiRelayApp(MDApp):
 
         except Exception:
 
+            self.command_in_progress = False
             self.rollback(old_level)
 
     def rollback(self, level):
