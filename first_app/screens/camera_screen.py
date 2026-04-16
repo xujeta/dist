327ad651cb1f9ea6c5a0f63718f40cd613d6995e
вишня <pnxx.py@gmail.com>
2026-04-16 09:44:52 +0300

    Update camera_screen.py

--- first_app/screens/camera_screen.py
+++ first_app/screens/camera_screen.py
@@ -1,8 +1,5 @@
 from kivy.uix.screenmanager import Screen
 
-class CameraScreen(Screen):
-    def open_gallery(self):
-        print("Галерея пока заглушка")
 
-    def go_next(self):
-        self.manager.parent.go_next("line")
+class CameraScreen(Screen):
+    pass
